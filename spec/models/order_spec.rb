f34a43e55dcc9a5ed0f261c79a83262863e61c16
DefyCab <love.bellinder@gmail.com>
2021-12-08 10:06:43 +0100

    all tests green and well formatted code

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,14 +1,22 @@
-# I have to format this code
-
 RSpec.describe Order, type: :model do
-  
   describe 'Database Table' do
-    it { is_expected.to have_db_column(:id).of_type(:integer) }
-    it { is_expected.to have_db_column(:user_id).of_type(:integer) }
+    it {
+      is_expected.to have_db_column(:id)
+        .of_type(:integer)
+    }
+    it {
+      is_expected.to have_db_column(:user_id)
+        .of_type(:integer)
+    }
   end
   describe 'Associations' do
-    it { is_expected.to belong_to(:user) }
-    it { is_expected.to have_many(:items).class_name('OrderItem') }
+    it {
+      is_expected.to belong_to(:user)
+    }
+    it {
+      is_expected.to have_many(:items)
+        .class_name('OrderItem')
+    }
   end
   describe 'Factory' do
     it 'is expected to be valid' do
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,17 +1,26 @@
-require "rails_helper"
-
 RSpec.describe Product, type: :model do
-  describe "Database Table" do
-    it { is_expected.to have_db_column(:id).of_type(:integer) }
-    it { is_expected.to have_db_column(:name).of_type(:string) }
-    it { is_expected.to have_db_column(:price).of_type(:float) }
+  describe 'Database Table' do
+    it {
+      is_expected.to have_db_column(:id)
+        .of_type(:integer)
+    }
+    it {
+      is_expected.to have_db_column(:name)
+        .of_type(:string)
+    }
+    it {
+      is_expected.to have_db_column(:price)
+        .of_type(:float)
+    }
   end
- # describe 'Associations' do
+  # describe 'Associations' do
   #   it { is_expected.to belong_to(:user) }
   # end
-  describe "Factory" do
-    it "is expected to be valid" do
+  describe 'Factory' do
+    it 'is expected to be valid' do
       expect(create(:product)).to be_valid
     end
   end
 end
+
+
